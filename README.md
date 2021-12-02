1aba07429baeeb0564d1d265f1466df33427df90
LuizFelipePinho <luizfelipesilva458@gmail.com>
2021-12-01 22:49:17 -0300

    initial project

---
+++ README.md
@@ -0,0 +1,2 @@
+# back-end-brechoTech
+alterações feitas
---
+++ src/app.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+
+@Module({
+  // no imports chamamos o modulo de cada componente
+  imports: [],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
