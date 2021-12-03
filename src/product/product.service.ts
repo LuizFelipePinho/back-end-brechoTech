c68cc3537aaeae3bd7fbd30e6f29f7caebf55b65
LuizFelipePinho <luizfelipesilva458@gmail.com>
2021-12-02 22:52:59 -0300

    route to create product

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,14 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { CreateProductDto } from './dto/product.dto';
-import { Prisma, Product } from '@prisma/client';
+import { Product } from '@prisma/client';
 import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
 export class ProductService {
   constructor(private db: PrismaService) {}
 
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+  async create(createProductDto: CreateProductDto) {
+    const data = createProductDto;
+    const createProduct = await this.db.product.create({ data });
+
+    return createProduct;
   }
 
   async findAll(): Promise<Product[]> {
