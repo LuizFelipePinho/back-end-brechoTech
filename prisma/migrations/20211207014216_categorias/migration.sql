c43a1ebb771e47348975aba549869f183b9c18b3
Pedro Santos <90013568+PedroSantoss-Dev@users.noreply.github.com>
2021-12-06 22:50:58 -0300

    category

--- prisma/migrations/20211207014216_categorias/migration.sql
+++ prisma/migrations/20211207014216_categorias/migration.sql
@@ -11,3 +11,16 @@ CREATE TABLE "Product" (
 
     CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateTable
+CREATE TABLE "Categorias" (
+    "id" TEXT NOT NULL,
+    "hardwerEntrada" TEXT,
+    "hardwerSaida" TEXT,
+    "hardwerProcessamento" TEXT,
+    "hardwerArmazenamento" TEXT,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Categorias_pkey" PRIMARY KEY ("id")
+);
