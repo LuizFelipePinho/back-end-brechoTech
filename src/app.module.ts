6ea562352d96d8490facf1e5c9ef3ae313c006d4
Pedro Santos <90013568+PedroSantoss-Dev@users.noreply.github.com>
2021-12-07 20:40:22 -0300

    add crud salesman

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ProductModule } from './product/product.module';
 import { CategoriasModule } from './categorias/categorias.module';
+import { VendedorModule } from './vendedor/vendedor.module';
 @Module({
   // no imports chamamos o modulo de cada componente
-  imports: [ProductModule, CategoriasModule],
+  imports: [ProductModule, CategoriasModule, VendedorModule],
   controllers: [],
   providers: [],
 })
